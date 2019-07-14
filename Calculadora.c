206e7c97af6bf3b6c051237b310aad9a0c32a4ec
victorgmassimi <victorgmassimi@hotmail.com>
2019-07-14 18:32:05 -0500

    update calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+#define MAX_LENGTH 51
+
+typedef struct
+{
+	char nombre[MAX_LENGTH];
+	long int telefono;
+	char e_mail[MAX_LENGTH]; 
+}persona;
 
 void suma()
 {
@@ -56,14 +67,51 @@ void resta()
 	printf("El resultado es: %d\n",resta);
 }
 
-int main()
+void divicion()
+{
+	float divisor = 1, dividendo = 1;
+	char continuar[2];
+	continuar[0] = 's';
+	do
+	{
+		printf("Ingresa el valor a dividir: ");
+		scanf("%f",&dividendo);
+		printf("Ingresa un valor por el que dividir: ");
+		scanf("%f",&divisor);
+
+		printf("El resultado es: %f\nRealizar nueva operacion [s/n]: ",(dividendo / divisor));
+		scanf("%c",&continuar[0]);
+		if (getchar() != '\n');
+
+	}while(continuar[0] = 's');
+}
+
+void multiplicacion()
+{
+	int a = 1,b = 1;
+	char continuar[2];
+	continuar[0] = 's';
+	do
+	{
+		printf("Ingresa el valor a multiplicar: ");
+		scanf("%f",&a);
+		printf("Ingresa un valor por el que multiplicar: ");
+		scanf("%f",&b);
+
+		printf("El resultado es: %f\nRealizar nueva operacion [s/n]: ",(a * b));
+		scanf("%c",&continuar[0]);
+
+	}while(continuar[0] = 's');
+}
+
+void calculadora()
 {
 	char operacion[15],continuar[2];
 	do
 	{
 		
 		continuar[0] = 's';
-		printf("Operacion a realizar: suma, resta\n");
+		printf("Operacion a realizar: suma, resta, divicion, multiplicacion\n");
 		scanf("%s",&operacion[0]);
 	
 	
@@ -80,6 +128,12 @@ int main()
 			case 'R':
 				resta();
 				break;
+			case 'd':
+			case 'D':
+				divicion();
+			case 'm':
+			case 'M':
+				multiplicacion();
 		}
 
 		printf("hacer otra operacion? [s/n]: ");
@@ -93,5 +147,52 @@ int main()
 	sleep(5);
 
 	printf("\n\e[2J\e[H");
-	return 0;
+}
+
+void ingresar_contactos()
+{
+	int cantidad_datos = 1;
+	FILE *file;
+
+	file = fopen("Agenda.txt","a+");
+	if (file == NULL)
+		{
+			printf("ERROR OPENING FILE\n");
+			exit (1);
+		}
+
+	printf("Cuantos contactos se van a guardar: ");
+	scanf("%d",&cantidad_datos);
+
+	persona contactos[cantidad_datos];
+
+	for(int i = 0; i < cantidad_datos; i++)
+	{
+		printf("Ingresa nombre:\n");
+		fgets(contactos[i].nombre,MAX_LENGTH-1,stdin);
+		fprintf(file, "%s\n",contactos[i].nombre);
+		printf("Ingresa telefono: ");
+		scanf("%ld",&contactos[i].telefono);
+		if(getchar() != '\n');
+		fprintf(file, "%ld\n",contactos[i].telefono);
+		printf("Ingresa correo electronico:\n");
+		fgets(contactos[i].e_mail,MAX_LENGTH-1,stdin);
+		fprintf(file, "%s\n",contactos[i].e_mail);
+	}
+
+fclose(file);
+
+}
+void buscar_contactos()
+{
+	FILE *file;
+	file = fopen("Agenda.txt","r");
+	if (file == NULL)
+	{
+		printf("ERROR OPENING FILE\n");
+		exit (1);
+	}
+	
+
+
 }
