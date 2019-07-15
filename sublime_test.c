3553e29ae6b2dd7b10b121fb104aca8861eb58c2
victorgmassimi <victorgmassimi@hotmail.com>
2019-07-14 19:31:39 -0500

    update sublime_test.c

--- sublime_test.c
+++ sublime_test.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
+typedef struct
+{
+	char nombre[15];
+	int telefono;
+
+}test;
 int main()
 {
-	printf("Hello world\n");
+	FILE *file;
+	test *a;
+	test string_1[2];
+	test string_2[2];
+	strcpy(string_1[0].nombre, "Hello_world");
+	strcpy(string_2[0].nombre, "world");
+	string_1[0].telefono = 111111;
+	string_2[0].telefono = 222222;
+
+	file=fopen("Test.txt","a+");
+	if(!file)
+	{
+		perror("ERROR OPENING FILE\n");
+		exit (1);
+	}
+	fwrite()
+
+	
+
+
 
 	return 0;
 }
